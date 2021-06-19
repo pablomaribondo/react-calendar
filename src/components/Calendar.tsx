056500cc4dc9321119ef51560d3295e68da06e43
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-19 17:03:26 -0300

    feat: create calendar component

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,72 @@
+import { FC } from 'react';
+
+import useCalendar, { Column } from '../hooks/useCalendar';
+
+const Calendar: FC = () => {
+  const {
+    calendarRows,
+    selectedDate,
+    todayFormatted,
+    days,
+    months,
+    getNextMonth,
+    getPrevMonth
+  } = useCalendar();
+
+  const dateClickHandler = (date: string) => {
+    console.log(date);
+  };
+
+  return (
+    <>
+      <p>
+        Selected Month:{' '}
+        {`${months[selectedDate.getMonth()]} - ${selectedDate.getFullYear()}`}{' '}
+      </p>
+
+      <table className="table">
+        <thead>
+          <tr>
+            {days.map(day => (
+              <th key={day}>{day}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {Object.values(calendarRows).map((cols: Column[]) => (
+            <tr key={cols[0].date}>
+              {cols.map(col =>
+                col.date === todayFormatted ? (
+                  <td
+                    key={col.date}
+                    className={`${col.classes} today`}
+                    onClick={() => dateClickHandler(col.date)}
+                  >
+                    {col.value}
+                  </td>
+                ) : (
+                  <td
+                    key={col.date}
+                    className={col.classes}
+                    onClick={() => dateClickHandler(col.date)}
+                  >
+                    {col.value}
+                  </td>
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <button type="button" className="button" onClick={getPrevMonth}>
+        Prev
+      </button>
+      <button type="button" className="button" onClick={getNextMonth}>
+        Prev
+      </button>
+    </>
+  );
+};
+
+export default Calendar;
